3c4767c2ff747ee6cdaec251ded13f20a40267ed
Aleksey242424 <alekseysokolovsky_256@mail.ru>
2024-04-13 16:19:36 +0300

    add connect frontend for auth

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+app.mount("/auth/static", StaticFiles(directory="src/auth/static"), name="static_auth")
 
 from src.auth import auth
 app.include_router(auth)
--- src/auth/views.py
+++ src/auth/views.py
@@ -5,8 +5,13 @@ from fastapi import Request,Depends
 from src.auth.schemes import LoginForm,RegisterForm
 from src.auth.user import Users
 from jwt import decode,encode
+from fastapi.staticfiles import StaticFiles
+
+
 
 templates = Jinja2Templates(directory="src/auth/templates")
+
+
 
 @route.get("/")
 @route.get("/login/")
@@ -19,11 +24,11 @@ async def login(request:Request):
 @route.post("/login/")
 async def login(data:LoginForm = Depends(LoginForm.as_form)):
     user = await Users.get_instance(username=data.username,password=data.password)
-    return {"data":{
-        "username":user.username,
-        "password":user.password
-    }}
-
+    token = encode({"username":user.username,"password":user.password},key="secret",algorithm="HS256")
+    response = RedirectResponse(f"/auth/tracker/",status_code=302)
+    response.set_cookie("user",token)
+    return response
+    
 @route.get("/register/")
 async def register(request:Request):
     return templates.TemplateResponse(request,"auth/register.html")
@@ -38,24 +43,29 @@ async def register_p(data:RegisterForm=Depends(RegisterForm.as_form)):
                     worker_type=data.worker_type)
     user = await Users.get_instance(username=data.username,
                                     password=data.password)
-    response = RedirectResponse("/auth/tracker/",status_code=302)
     token = encode({"username":user.username,"password":user.password},key="secret",algorithm="HS256")
+    response = RedirectResponse(f"/auth/tracker/",status_code=302)
+    
     response.set_cookie("user",token)
     return response
 
 @route.get("/tracker/")
 async def tracker(request:Request):
     data = request.cookies.get('user')
-    data = decode(data,key="secret",algorithms=["HS256"])
-    user = await Users.get_instance(username=data["username"],
+    if data:
+    
+        data = decode(data,key="secret",algorithms=["HS256"])
+        user = await Users.get_instance(username=data["username"],
                                     password=data["password"])
-    return {"data":{
-        "username":user.username,
-        "password":user.password,
-        "email":user.email,
-        "is_owner":user.is_owner,
-        "worker_type":user.worker_type
-    }} 
+        return {"data":{
+            "username":user.username,
+            "password":user.password,
+            "email":user.email,
+            "is_owner":user.is_owner,
+            "worker_type":user.worker_type
+        }} 
+    return RedirectResponse(url="/auth/login/")
+
 @route.post("/tracker/")
 async def tracker_p():
     pass
